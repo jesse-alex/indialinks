93e3d562281d2e56107a8bf6f80162b52a339878
Jesse Alex <jesse.j.alex@gmail.com>
2015-08-29 18:03:40 +0530

    1.4.4.3
    FIXES
    ——-
    * List - Padding adjusted
    * Branding - Replaced Logo
    * Navbar/Header - Changed Header to #004164 and created navbar with the
    original colours.
    * Footer - Changed Footer to #004164
    * Fixed Hover button
    * Aligned Navbar menus to centre and aligned drop down menus accordingly
    * Copy all to 14px
    * Dropdown changed font to 15px
    
    PAGES CREATED
    ———————-
    Domain Register 2

--- site/js/custom.js
+++ site/js/custom.js
@@ -14,3 +14,21 @@ $(document).ready(function(){
 $(document).ready(function(){
     $('[data-toggle=tooltip-active]').tooltip({trigger: 'manual'}).tooltip('show');
 });
+
+jQuery(function($) {
+
+    $('[data-toggle=tooltip]').hover(function() {
+        $('[data-toggle=tooltip-active]').tooltip('destroy'); 
+    }, function() {
+        $('[data-toggle=tooltip-active]').tooltip({trigger: 'hover'}); 
+    });
+
+});
+
+
+// SMOOTHER HOVER ON NAVBAR
+//jQuery('ul.nav li.dropdown').hover(function() {
+//  jQuery(this).find('.dropdown-menu').stop(true, true).delay(200).fadeIn();
+//}, function() {
+//  jQuery(this).find('.dropdown-menu').stop(true, true).delay(200).fadeOut();
+//});
